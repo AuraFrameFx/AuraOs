eed99882f7f596c66e8c0684241d780dd2e7da8e
AuraKai <wehttam1989@gmail.com>
2025-08-04 01:48:35 -0600

    1

---
+++ buildSrc/src/main/kotlin/AndroidAppConventionPlugin.kt
@@ -0,0 +1,4 @@
+// REMOVED: This duplicate file was causing conflicts
+// The actual AndroidAppConventionPlugin is properly located in:
+// buildSrc/src/main/kotlin/plugins/AndroidAppConventionPlugin.kt
+
